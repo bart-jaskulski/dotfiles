a69d76e39a124c6d30a6e35dbef45d4202a76de4
Bart Jaskulski <bjaskulski@protonmail.com>
2022-05-02 22:10:39 +0200

    Remove nvim config. Update i3.

--- .config/i3/load_layout.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Append PhpStorm to workspace 2 and 3
-i3-msg "workspace 3; append_layout ./.config/i3/workspace-term.json"
-i3-msg "workspace 2; append_layout ~/.config/i3/workspace-dev.json"
-
-# Run the PhpStorm to fill placeholders
-bash -c ~/bin/PhpStorm/bin/phpstorm.sh
--- .config/nvim/lua/plugins/symbols.lua
+++
@@ -1,9 +0,0 @@
-vim.g.symbols_outline = {
-	position = 'left',
-	show_numbers = true,
-	show_guides = false,
-	auto_preview = false,
-	symbols = {
-		Key = {icon = "P", hl = "TSType" }
-	}
-}
